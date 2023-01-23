1a960124a3395a8b6588f77513e22a98cd6decdf
Ben <vvvvvben@icloud.com>
2023-01-23 12:29:46 +0800

    common module优化

--- common/src/main/java/cn/niu/common/message/ChatResponseMessage.java
+++ common/src/main/java/cn/niu/common/message/ChatResponseMessage.java
@@ -3,11 +3,23 @@ package cn.niu.common.message;
 import lombok.Data;
 import lombok.ToString;
 
+/**
+ * 私信回复消息
+ *
+ * @author Ben
+ */
 @Data
 @ToString(callSuper = true)
 public class ChatResponseMessage extends AbstractResponseMessage {
 
+    /**
+     * 发送方username
+     */
     private String from;
+
+    /**
+     * 内容
+     */
     private String content;
 
     public ChatResponseMessage(boolean success, String reason) {
